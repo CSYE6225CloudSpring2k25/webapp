b2f379732d98a322c6869a009a1f73e13402419a
iyashsaraf <saraf.y@northeastern.edu>
2025-02-05 19:17:23 -0500

    added invalid URL error and added a fix for jest not getting stopped even after running all unit tests

--- src/app.js
+++ src/app.js
@@ -6,11 +6,12 @@ const { sequelize } = require('./Entities');
 app.use(express.json());
 
 app.use('/healthz', healthzRoute);
-
+if (process.env.NODE_ENV !== 'test') {
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+}
 
 sequelize
   .sync()
@@ -18,8 +19,15 @@ sequelize
     console.log('Database connected and synced');
   })
   .catch((err) => {
-    console.error('Error connecting to the database:', err);
+    console.log('Error connecting to the database');
   });
+app.use((req, res) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  console.log('Error:', 'Invalid URL');
+  res.status(404).send();
+});
 app.use((err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
     console.error('Error:', 'Invalid JSON detected in the request body');
@@ -27,4 +35,6 @@ app.use((err, req, res, next) => {
   }
   next(err);
 });
+
+module.exports = app;
 
